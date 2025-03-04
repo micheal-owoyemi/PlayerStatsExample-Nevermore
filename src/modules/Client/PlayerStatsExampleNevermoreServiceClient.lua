36d27dca63b0758689dccf9a90726a9989fcf418
Micheal Owoyemi <120283182+micheal-owoyemi@users.noreply.github.com>
2025-03-03 22:18:29 -0600

    Stats UI
    StatsUI Blend Object

--- src/modules/Client/PlayerStatsExampleNevermoreServiceClient.lua
+++ src/modules/Client/PlayerStatsExampleNevermoreServiceClient.lua
@@ -3,6 +3,18 @@
 ]=]
 
 local require = require(script.Parent.loader).load(script)
+local Blend = require("Blend")
+local Maid = require("Maid")
+
+local statObservables = {
+    Points = Blend.State(2),
+    Assists = Blend.State(0),
+    Spikes = Blend.State(6),
+    Blocks = Blend.State(0),
+    Sets = Blend.State(1),
+    Dives = Blend.State(0),
+    Recieves = Blend.State(7),
+}
 
 local PlayerStatsExampleNevermoreServiceClient = {}
 PlayerStatsExampleNevermoreServiceClient.ServiceName = "PlayerStatsExampleNevermoreServiceClient"
@@ -10,9 +22,7 @@ PlayerStatsExampleNevermoreServiceClient.ServiceName = "PlayerStatsExampleNeverm
 function PlayerStatsExampleNevermoreServiceClient:Init(serviceBag)
 	assert(not self._serviceBag, "Already initialized")
 	self._serviceBag = assert(serviceBag, "No serviceBag")
-
-	-- External
-	self._serviceBag:GetService(require("CmdrServiceClient"))
+	self._maid = Maid.new()
 
 	-- Internal
 	self._serviceBag:GetService(require("PlayerStatsExampleNevermoreTranslator"))
@@ -20,6 +30,105 @@ end
 
 function PlayerStatsExampleNevermoreServiceClient:Start()
 	print("PlayerStatsExampleNevermoreServiceClient started")
+	self:InitStatsUI()
+end
+
+function PlayerStatsExampleNevermoreServiceClient:InitStatsUI()
+	local statsUI = self:ConstructStatsUI()
+
+	self._maid:GiveTask(statsUI:Subscribe(function(gui)
+		self:PopulateStatsUI(gui.Container.Background.StatsContainer, statObservables)
+	end))
+end
+
+function PlayerStatsExampleNevermoreServiceClient:ConstructStatsUI()
+	local render = Blend.New "ScreenGui" {
+		Parent = game.Players.LocalPlayer.PlayerGui,
+		IgnoreGuiInset = true,
+		ResetOnSpawn = false,
+		Name = "PlayerStatsGUI",
+	
+		Blend.New "Frame" {
+			Size = UDim2.new(0.25, 0, 0.5, 0),
+			Position = UDim2.new(0.1, 0, 0.25, 0),
+			BackgroundTransparency = 1,
+			Name = "Container",
+
+			Blend.New "Frame" {
+				Size = UDim2.new(1, 0, 1, 0),
+				Position = UDim2.new(0, 0, 0, 0),
+				Name = "Background",
+				BackgroundColor3 = Color3.fromRGB(255, 255, 255),
+
+				Blend.New "UIStroke" {
+					Thickness = 4,
+					Color = Color3.fromRGB(0, 0, 0),
+				},
+
+				Blend.New "TextLabel" {
+					Size = UDim2.new(1, 0, 0.1, 0),
+					Position = UDim2.new(0, 0, 0, 0),
+					Name = "Title",
+					BackgroundTransparency = 1,
+					Text = "STATS",
+					TextColor3 = Color3.fromRGB(0, 0, 0),
+					TextSize = 40,
+					Font = Enum.Font.SourceSansBold,
+				},
+
+				Blend.New "ScrollingFrame" {
+					Size = UDim2.new(1, 0, 0.9, 0),
+					Position = UDim2.new(0, 0, 0.1, 0),
+					Name = "StatsContainer",
+					BackgroundTransparency = 1,
+					ScrollBarThickness = 8,
+					ScrollingDirection = Enum.ScrollingDirection.Y,
+					AutomaticCanvasSize = Enum.AutomaticSize.Y,
+
+					Blend.New "UIListLayout" {
+						SortOrder = Enum.SortOrder.LayoutOrder,
+						Padding = UDim.new(0, 10),
+					},
+				},
+			}
+		}
+	}
+
+	return render
+end
+
+function PlayerStatsExampleNevermoreServiceClient:ConstructStatCell(key, value)
+	if not (key and value) then
+		return
+	end
+
+	local statCellRender = Blend.New "TextButton" {
+		Size = UDim2.new(0.5, 0, 0.1, 0),
+		Position = UDim2.new(0, 0, 0, 0),
+		Name = key.." StatCell",
+		BackgroundTransparency = 1,
+		Text = key .. ": " .. value,
+		TextColor3 = Color3.fromRGB(0, 0, 0),
+		TextSize = 20,
+		Font = Enum.Font.SourceSans,
+	}
+
+	print("Constructed stat cell", key, value)
+	return statCellRender
+end
+
+function PlayerStatsExampleNevermoreServiceClient:PopulateStatsUI(guiContainer: ScrollingFrame, stats: table)
+	if not (guiContainer and stats) then
+		return
+	end
+
+	for key, stat in (stats) do
+		local statFrame = self:ConstructStatCell(key, stat.Value)
+
+		self._maid:GiveTask(statFrame:Subscribe(function(gui)
+			gui.Parent = guiContainer
+		end))
+	end
 end
 
 return PlayerStatsExampleNevermoreServiceClient
--- src/modules/Server/PlayerStatsExampleNevermoreService.lua
+++ src/modules/Server/PlayerStatsExampleNevermoreService.lua
@@ -3,7 +3,6 @@
 ]=]
 
 local require = require(script.Parent.loader).load(script)
-local Blend = require("Blend")
 
 local PlayerStatsExampleNevermoreService = {}
 PlayerStatsExampleNevermoreService.ServiceName = "PlayerStatsExampleNevermoreService"
@@ -12,9 +11,6 @@ function PlayerStatsExampleNevermoreService:Init(serviceBag)
 	assert(not self._serviceBag, "Already initialized")
 	self._serviceBag = assert(serviceBag, "No serviceBag")
 
-	-- External
-	self._serviceBag:GetService(require("CmdrService"))
-
 	-- Internal
 	self._serviceBag:GetService(require("PlayerStatsExampleNevermoreTranslator"))
 end
@@ -22,12 +18,6 @@ end
 
 function PlayerStatsExampleNevermoreService:Start()
 	print("PlayerStatsExampleNevermoreService started")
-end
-
-function PlayerStatsExampleNevermoreService:CreateStatBlendObject(observableStatMap)
-    assert(type(observableStatMap) == "table", "Expected observableStatMap to be a table")
-    local statBinding = Blend.Object(observableStatMap)
-    return statBinding
 end
 
 return PlayerStatsExampleNevermoreService
